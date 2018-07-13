e365ccf028c4d7f02dce7258c3cffd29296f58e5
suntao <384631173@qq.com>
2018-07-13 11:30:57 +0800

    引用别人github jar 实现elastic job 定时任务

--- src/main/java/com/foxiaotao/test/elasticjobspringbootstarter/config/MySimpleJob.java
+++ src/main/java/com/foxiaotao/test/elasticjobspringbootstarter/config/MySimpleJob.java
@@ -2,6 +2,7 @@ package com.foxiaotao.test.elasticjobspringbootstarter.config;
 
 import com.dangdang.ddframe.job.api.ShardingContext;
 import com.dangdang.ddframe.job.api.simple.SimpleJob;
+import com.dangdang.elasticjob.lite.annotation.ElasticSimpleJob;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -10,11 +11,11 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @Slf4j
-
+@ElasticSimpleJob(cron = "* * * * * ?")
 public class MySimpleJob implements SimpleJob {
 
     @Override
     public void execute(ShardingContext shardingContext) {
-
+        log.info("itemId={}", shardingContext.getShardingItem());
     }
 }
--- src/main/java/com/foxiaotao/test/elasticjobspringbootstarter/controller/HomeController.java
+++ src/main/java/com/foxiaotao/test/elasticjobspringbootstarter/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.foxiaotao.test.elasticjobspringbootstarter.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sxt.com.simon.password.des.SimonPassword;
 
 /**
  * Created with suntao on 2018/7/12
@@ -15,6 +16,7 @@ public class HomeController {
     @RequestMapping("/home")
     public void home(){
         log.info("====home====");
+        log.info("[password]{}",SimonPassword.enPassword("12233"));
         return ;
     }
 }
