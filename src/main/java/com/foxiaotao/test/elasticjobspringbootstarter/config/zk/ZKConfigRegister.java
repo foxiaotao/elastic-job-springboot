99e0880aaafb9fc98fcc9edb84a55895f211d3f0
suntao <384631173@qq.com>
2018-07-13 17:06:57 +0800

    动态修改任务属性

---
+++ src/main/java/com/foxiaotao/test/elasticjobspringbootstarter/config/zk/ZKConfigRegister.java
@@ -0,0 +1,19 @@
+package com.foxiaotao.test.elasticjobspringbootstarter.config.zk;
+
+import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperConfiguration;
+import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created with suntao on 2018/7/13
+ */
+@Configuration
+public class ZKConfigRegister {
+
+    @Bean(initMethod = "init", name = "zookeeperRegistryCenter")
+    public ZookeeperRegistryCenter zookeeperRegistryCenter(@Value("${elaticjob.zookeeper.server-lists}")final String zkServerList, @Value("${elaticjob.zookeeper.namespace}") final String namespace) {
+        return new ZookeeperRegistryCenter(new ZookeeperConfiguration(zkServerList, namespace));
+    }
+}
---
+++ src/main/java/com/foxiaotao/test/elasticjobspringbootstarter/controller/DynamicJobController.java
@@ -0,0 +1,48 @@
+package com.foxiaotao.test.elasticjobspringbootstarter.controller;
+
+import com.dangdang.ddframe.job.config.JobCoreConfiguration;
+import com.dangdang.ddframe.job.config.simple.SimpleJobConfiguration;
+import com.dangdang.ddframe.job.lite.api.JobScheduler;
+import com.dangdang.ddframe.job.lite.config.LiteJobConfiguration;
+import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
+import com.foxiaotao.test.elasticjobspringbootstarter.config.MySimpleJob;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sxt.com.simon.password.des.SimonPassword;
+
+import javax.annotation.Resource;
+import java.util.UUID;
+
+/**
+ * Created with suntao on 2018/7/12
+ */
+@RestController
+@Slf4j
+public class DynamicJobController {
+
+
+    @Resource(name = "zookeeperRegistryCenter")
+    private ZookeeperRegistryCenter zookeeperRegistryCenter;
+
+    @RequestMapping("/dynamic_job")
+    public String home(){
+
+        int shardingTotalCount = 2;
+        String jobName = UUID.randomUUID().toString() + "-test";
+        JobCoreConfiguration jobCoreConfiguration = JobCoreConfiguration.newBuilder(jobName, "* * * * * ?", shardingTotalCount).shardingItemParameters("0=A,1=B").build();
+
+        SimpleJobConfiguration simpleJobConfiguration = new SimpleJobConfiguration(jobCoreConfiguration, MySimpleJob.class.getCanonicalName());
+
+        JobScheduler jobScheduler = new JobScheduler(zookeeperRegistryCenter, LiteJobConfiguration.newBuilder(simpleJobConfiguration).build());
+        try {
+            jobScheduler.init();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("定时任务创建失败");
+        }
+
+        return "success";
+    }
+}
